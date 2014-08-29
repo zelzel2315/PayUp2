e093e045524e265b3a5581532ad9eb1fd8ddc676
eganpg <eganpg@gmail.com>
2014-08-29 15:00:40 -0700

    adding some stuff trying to back up, ahhh help I'm scared

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ PayUp::Application.routes.draw do
 
   resources :users
   
-  get '/venmo' => 'venmo#index'
-  get '/venmo/new' => 'venmo#new', as: :new
+  get '/venmo' => 'venmo#home'
+  get '/venmo/home' => 'venmo#home', as: :home
 
 
   # The priority is based upon order of creation: first created -> highest priority.

8a0c0080e43727b6c6a59c893b004ab6293dc86b
zelzel2315 <criselda.valderrama@gmail.com>
2014-08-29 09:52:24 -0700

    more views and bootstrap added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 PayUp::Application.routes.draw do
+
+  resources :users
+  
+  get '/venmo' => 'venmo#venmo'
+  get '/venmo/feed' => 'venmo#feed', as: :feed
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

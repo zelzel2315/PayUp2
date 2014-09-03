b656ba7d2057de56959470b7866c24b56c2ce53b
zelzel2315 <criselda.valderrama@gmail.com>
2014-09-03 16:01:19 -0700

    added angular gems

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@
   resources :users, defaults: { format: :json }
   resources :bets, defaults: { format: :json }
   
-
+  
   get '/venmo/home' => 'venmo#home', as: :home
   post '/venmo/home' => 'venmo#bet'
+  get 'venmos' => 'venmo#index'
 
 
   root 'users#index'

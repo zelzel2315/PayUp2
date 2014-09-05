341ace613c813c9e3db287ed21d88bff353a2837
eganpg <eganpg@gmail.com>
2014-09-04 18:19:35 -0700

    we can now make a bet that saves in the db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,12 +22,14 @@
     user = @response["user"]
     @access_token = @response["access_token"]
     @refresh_token = @response["refresh_token"]
-    friends = "https://api.venmo.com/v1/users/" + user["id"] + "/friends?"
-    @retfriend = HTTParty.get(friends, :query => { "access_token" => @access_token})
-    @data = @retfriend["data"]
+    # friends = "https://api.venmo.com/v1/users/" + user["id"] + "/friends?"
+    # @retfriend = HTTParty.get(friends, :query => { "access_token" => @access_token})
+    # @data = @retfriend["data"]
     @user = User.new(:username => user["username"], :first_name => user["first_name"], :last_name => user["last_name"], :display_name => user["display_name"], :is_friend => user["is_friend"], :friends_count => user["friends_count"], :about => @response["access_token"], :email => user["email"], :phone => user["phone"], :profile_picture_url => user["profile_picture_url"], :friend_request => user["friend_request"], :trust_request => user["trust_request"], :venmo_id => user["id"], :date_joined => user["date_joined"] )
-    @bet = Bet.new
+    
     if @user.save
+      @current_user = @user.id
+      puts @current_user
       render new_user_path
     end
   end

f70eb6e4d3bce32407a76553fefffd3e11953fb9
eganpg <eganpg@gmail.com>
2014-09-04 19:21:04 -0700

    new add a bet form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140904014806) do
+ActiveRecord::Schema.define(version: 20140905013506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20140904014806) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "is_accepted"
+    t.string   "challenger"
+    t.string   "challengee"
   end
 
   create_table "user_bets", force: true do |t|
